b5c7c1ae6a8b35b19dbed623c11ad81172004b2a
thekiana <kianakdonaldson@gmail.com>
2019-10-21 12:09:39 -0700

    Implement progress on object case

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -28,4 +28,29 @@ var stringifyJSON = function (obj) {
 
     return '[' + result + ']';
   }
+
+  if (typeof obj === "object") {
+    debugger;
+    var result = "";
+
+    if (Object.keys(obj).length === 0) {
+      return "{}";
+    }
+
+    for (var key in obj) {
+      var value = obj[key];
+
+      if (Object.keys(obj).length === 1) {
+        result += `"${key}":${stringifyJSON(value)}`;
+      }
+
+      result += `"${key}":${stringifyJSON(value)}, `;
+    }
+
+    return "{" + result + "}";
+  }
 };
+
+// ideas to fix line 47
+// var keyArr = Object.keys(obj);
+// var lastEle = keyArr[keyArr.length - 1];
