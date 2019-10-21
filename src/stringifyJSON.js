a27baa380872258f3077ced2fcb1785f4e15fce9
thekiana <kianakdonaldson@gmail.com>
2019-10-21 11:27:06 -0700

    Implement functionality through value type of array

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,29 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function (obj) {
+
+  if (typeof obj === "number") {
+    return "" + obj;
+  } else if (typeof obj === "string") {
+    return '"' + obj + '"';
+  } else if (typeof obj === "boolean") {
+    return "" + obj;
+  } else if (obj === null) {
+    return "" + null;
+  }
+
+  if (Array.isArray(obj)) {
+    var result = [];
+
+    if (obj.length === 0) {
+      return "[]";
+    }
+
+    for (var i = 0; i < obj.length; i++) {
+      result.push(stringifyJSON(obj[i]));
+    }
+
+    return '[' + result + ']';
+  }
 };
