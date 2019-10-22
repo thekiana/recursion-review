eb6b11cb8cf841292e39613c36491283bdc7663b
thekiana <kianakdonaldson@gmail.com>
2019-10-21 20:08:28 -0700

    Implement functionality from scratch

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,26 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function (className) {
+  var body = document.body;
+  var elementList = [];
+
+  var classCheck = function (element) {
+
+    if (element.classList && element.classList.contains(className)) {
+      elementList.push(element);
+    }
+    var children = element.childNodes;
+
+    for (var i = 0; i < children.length; i++) {
+      classCheck(children[i]);
+    }
+
+    return elementList;
+  }
+
+
+  classCheck(body);
+  return elementList;
+
 };
